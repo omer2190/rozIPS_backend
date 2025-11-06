367e9afe256283ddf0966a6b70a734df30c83c60
omer2190 <44607297+omer2190@users.noreply.github.com>
2025-11-06 16:34:17 +0300

    Update upload.routes.ts

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { uploadImage } from "../controllers/upload.controller";
-import upload from "../middlewares/upload.middleware";
+// import upload from "../middlewares/upload.middleware";
 import { auth } from "../middlewares/auth.middleware";
 
 const router = Router();
@@ -8,6 +8,6 @@ const router = Router();
 // @route   POST api/upload
 // @desc    Upload an image
 // @access  Private
-router.post("/", auth, upload.single("image"), uploadImage);
+// router.post("/", auth, upload.single("image"), uploadImage);
 
 export default router;
